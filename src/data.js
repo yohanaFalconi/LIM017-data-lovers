4afb9d2da80643e3cc4fd86848a5b27780b5f0c5
yohanaFalconi <yohanafalconivivas@gmail.com>
2022-03-12 23:19:23 -0500

    estadÃísticos y un poco de css

--- src/data.js
+++ src/data.js
@@ -36,3 +36,28 @@ export function orderBy(data,clicked){
   }
   return newArray; 
 }
+
+// funcion para top10 segun puntos de combate
+export const topStats = (data, click) => {
+  let topTenPokemon = [];
+  switch(click) {
+    case 'base-attack':
+      topTenPokemon = data.sort((a, b) => (parseInt(b.stats['base-attack']) - parseInt(a.stats['base-attack']))).slice(0, 10);
+      break;
+    case 'base-defense':
+      topTenPokemon = data.sort((a, b) => (parseInt(b.stats['base-defense']) - parseInt(a.stats['base-defense']))).slice(0, 10);
+      break;
+    case 'base-stamina':
+      topTenPokemon = data.sort((a, b) => (parseInt(b.stats['base-stamina']) - parseInt(a.stats['base-stamina']))).slice(0, 10);
+      break;
+    case 'max-cp':
+      topTenPokemon = data.sort((a, b) => (parseInt(b.stats['max-cp']) - parseInt(a.stats['max-cp']))).slice(0, 10);
+      break;
+    case 'max-hp':
+      topTenPokemon = data.sort((a, b) => (parseInt(b.stats['max-hp']) - parseInt(a.stats['max-hp']))).slice(0, 10);
+      break;
+    default:
+      // do nothing
+  }
+  return topTenPokemon
+}
